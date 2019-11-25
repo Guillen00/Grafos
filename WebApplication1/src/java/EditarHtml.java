892f9d34b94102d018e4d2ef2df0d9ad2100e2df
Guillen00 <47611671+Guillen00@users.noreply.github.com>
2019-11-24 23:47:34 -0600

    Final

--- WebApplication1/src/java/EditarHtml.java
+++ WebApplication1/src/java/EditarHtml.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * En esta clase se copia el archivo original para editar uno secundario y se le añaden las listas de datos de notos y aristas 
  */
 
 import java.io.EOFException;
